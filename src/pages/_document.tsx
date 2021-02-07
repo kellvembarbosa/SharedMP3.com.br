b243f11ea0b9ba95614afbd4344351bba81fc6d4
kellvembarbosa <kellvembarbosa@icloud.com>
2021-02-07 14:40:32 +0000

    fixed chakra ui settings

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,33 @@ import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 import { ColorModeScript } from '@chakra-ui/react'
-import theme from './_theme'
+import { extendTheme } from "@chakra-ui/react"
+
+// 2. Add your color mode config
+const config = {
+    // initialColorMode: "light",
+    useSystemColorMode: true,
+    styles: {
+        global: {
+            "html": {
+                height: "100%",
+            },
+            "body": {
+                fontSize: "sm",
+                color: "gray.600",
+                lineHeight: "tall",
+                minHeight: "100%",
+                width: "100%",
+            },
+            a: {
+                color: "teal.500",
+            },
+        },
+    },
+}
+
+const theme = extendTheme({ config })
+
 export default class MyDocument extends Document {
     render() {
         return (
